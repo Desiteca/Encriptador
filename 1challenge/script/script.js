203c010d6700671530e27a1fa91b01a83e5ad1eb
Mabel Cuervo <mcuervo952@gmail.com>
2023-06-23 13:19:06 -0500

    termine el css de escritorio y el script

---
+++ 1challenge/script/script.js
@@ -0,0 +1,80 @@
+/* Encriptador/descriptador de texto que cambiara:
+ La letra "e" es convertida para "enter"
+ La letra "i" es convertida para "imes"
+ La letra "a" es convertida para "ai"
+ La letra "o" es convertida para "ober"
+ La letra "u" es convertida para "ufat"*/
+
+//Parte de encriptar el texto
+
+function encriptar() {
+
+    let frase=document.getElementById("mensaje").value;
+    console.log(frase);
+
+    if (frase) {
+        frase = frase.replaceAll("e", "enter");
+        frase = frase.replaceAll("i", "imes");
+        frase = frase.replaceAll("a", "ai");
+        frase = frase.replaceAll("o", "ober");
+        frase = frase.replaceAll("u", "ufat");
+    } else {
+
+    }
+
+    document.getElementById("cuadrodeTexto").style.display = "none";
+
+    document.getElementById("cuadroOculto").style.display = "block";
+    
+    let textoOculto = document.getElementById("textoOculto");
+
+    textoOculto.textContent = frase;
+
+    document.getElementById("copiar").style.display = "block";       
+        
+}
+
+//Parte de desencriptar el texto 
+
+function desencriptar() {
+
+    let frase=document.getElementById("mensaje").value;
+    console.log(frase);
+
+    if (frase) {
+        frase = frase.replaceAll("enter", "e");
+        frase = frase.replaceAll("imes", "i");
+        frase = frase.replaceAll("ai", "a");
+        frase = frase.replaceAll("ober", "o");
+        frase = frase.replaceAll("ufat", "u");
+    }
+    
+    document.getElementById("cuadrodeTexto").style.display = "none";
+
+    document.getElementById("cuadroOculto").style.display = "block";
+        
+    let textoOculto = document.getElementById("textoOculto");
+    
+    textoOculto.textContent = frase;
+    
+    document.getElementById("copiar").style.display = "block";      
+        
+}
+
+// Parte de copiar
+
+document.getElementById("copiar").style.display = "none";
+
+function copiar() {
+  
+    let elemento = document.getElementById("textoOculto");
+    let texto = elemento.textContent;
+
+    navigator.clipboard.writeText(texto).then(() => {
+          /* clipboard successfully set */
+    }, () => {
+          /* clipboard write failed */
+    });
+}
+  
+
